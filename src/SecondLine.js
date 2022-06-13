c1af10b0c09ff2326cd29c1931571a2a47df62e4
Kevin Christy Parinussa <kaysypy17th@gmail.com>
2022-06-13 15:44:24 +0700

    React 1st Day

---
+++ src/SecondLine.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class SecondLine extends Component{
+    render(){
+        return(
+            <form>
+                <input type="text" placeholder="Masukkan nama"/>
+            </form>
+        )
+    }
+}
+
+export default SecondLine;
